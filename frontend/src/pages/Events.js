d2cf38d00dfd98971fa8249f7adc2294f8f1d5d9
adnankhan <adnankhannad@gmail.com>
2024-05-29 22:59:31 +0500

    Handle response object in loader and handle error via loader

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -2,20 +2,26 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
+// Code inside of loader, Executes in the browser, not on the server
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events" };
+    throw { message: "Could not fetch events" };
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
